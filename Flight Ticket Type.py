1b304362ca44655195bfe7ca76088908379152ec
Jesse-Aggrey <115493961+Jesse-Aggrey@users.noreply.github.com>
2022-10-21 11:56:16 +0200

    Add files via upload

---
+++ Flight Ticket Type.py
@@ -0,0 +1,6 @@
+print("Hello and welcome, please indicate Priority or Non Priority ticket!")
+user = input("User? ")
+if user == "Priority":
+    print("Please follow the line to the right!")
+else:
+    print("Please follow the line to the left!")
