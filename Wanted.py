d0587f59afd57a5b482f400da5149d50b89a7019
Jesse-Aggrey <115493961+Jesse-Aggrey@users.noreply.github.com>
2022-10-21 12:19:54 +0200

    Add files via upload

---
+++ Wanted.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+city = input("Which city are you from?")
+postcode = input("What is your postcode?")
+age = input("What is your age? ")
+#height = input("What is your height?")
+
+if name == "Henry Floyd" and city == "London" and postcode == "SE12 5FU" and age == "27":
+    print("Please report yourself to your local GP")
+else:
+    print("You are cleared!")
